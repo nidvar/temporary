585b40525983b2ae42516929c5b3144ecbc0dc20
nidvar <djayshine87@hotmail.com>
2019-09-18 21:22:21 +1000

    first commit. title page and navbar

---
+++ Display.jsx
@@ -0,0 +1,10 @@
+class Display extends React.Component{
+    render(){
+        return(
+            <div>
+                <Nav />
+                <Main />
+            </div>
+        )
+    }
+}
---
+++ Nav.jsx
@@ -0,0 +1,14 @@
+class Nav extends React.Component{
+    render(){
+        return(
+            <div className='Nav'>
+                <h1>NIDVAR</h1>
+                <ul>
+                    <li><a href='#'>Apps</a></li>
+                    <li><a href='#'>Websites</a></li>
+                    <li><a href='#'>About</a></li>
+                </ul>
+            </div>
+        )
+    }
+}
