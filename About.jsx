cb368ba73701d3b40249a0b1549fdc456fb6971f
nidvar <djayshine87@hotmail.com>
2019-09-19 09:07:30 +1000

    about section

--- About.jsx
+++ About.jsx
@@ -1,8 +1,12 @@
 class About extends React.Component{
     render(){
         return(
-            <div>
-                <h1>About</h1>
+            <div className='About'>
+                <h3>Profile</h3>
+                <p>I graduated university with a bachelor degree in Science, majoring in Physics and Nanotechnology. I dabbled with HTML/CSS/PHP as a hobbie then focused heavily on JavaScript. Now I identify as a JavaScript developer working with React and Node.</p>
+
+                <h3>Current Status</h3>
+                <p>.....</p>
             </div>
         )
     }
